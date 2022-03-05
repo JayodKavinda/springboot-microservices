489f3ca44760f9b5861b69b058684da2b25aa4be
jayodkavinda <jayodkavinda@gmail.com>
2022-03-05 16:06:52 +0530

    add currency exchanger

--- currency-exchange-service/src/main/java/com/jyd/microservice/currencyexchangeservice/controllers/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/jyd/microservice/currencyexchangeservice/controllers/CurrencyExchangeController.java
@@ -1,4 +1,17 @@
 package com.jyd.microservice.currencyexchangeservice.controllers;
 
+import com.jyd.microservice.currencyexchangeservice.models.CurrencyExchange;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
 public class CurrencyExchangeController {
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyExchange retrieveValue(@PathVariable String from, @PathVariable String to){
+        return new CurrencyExchange(1000L,from, to, BigDecimal.valueOf(200));
+    }
 }
--- currency-exchange-service/src/main/java/com/jyd/microservice/currencyexchangeservice/models/CurrencyExchange.java
+++ currency-exchange-service/src/main/java/com/jyd/microservice/currencyexchangeservice/models/CurrencyExchange.java
@@ -1,4 +1,20 @@
 package com.jyd.microservice.currencyexchangeservice.models;
 
-public class CurrencyExhange {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor // need for JPA
+public class CurrencyExchange {
+
+    private Long id;
+    private String to;
+    private String form;
+    private BigDecimal conversionMultiple;
 }

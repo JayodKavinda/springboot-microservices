af85f4a3c28b2cb67d6a412e78c08459fc3d4be2
jayodkavinda <jayodkavinda@gmail.com>
2022-03-05 19:58:19 +0530

    update  currency convertor

--- currency-conversion-service/src/main/java/com/jyd/microservice/currencyconversionservice/controller/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/jyd/microservice/currencyconversionservice/controller/CurrencyConversionController.java
@@ -1,4 +1,29 @@
 package com.jyd.microservice.currencyconversionservice.controller;
 
+
+import com.jyd.microservice.currencyconversionservice.models.CurrencyConversion;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+
+@RestController
 public class CurrencyConversionController {
+
+    @GetMapping("/currency-conversion/form/{from}/to/{to}/quantity/{q}")
+    public CurrencyConversion calculateConversion(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal q){
+
+        HashMap<String, String> uriVariables =new HashMap<>();
+        uriVariables.put("from", from);
+        uriVariables.put("to", to);
+        ResponseEntity<CurrencyConversion> resEntity = new RestTemplate().getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversion.class, uriVariables);
+
+        CurrencyConversion currencyConversion = resEntity.getBody();
+
+        return new CurrencyConversion(currencyConversion.getId(),from, to, q, currencyConversion.getConversionMultiple(), q.multiply(currencyConversion.getConversionMultiple()),currencyConversion.getEnvironment());
+    }
 }
--- currency-conversion-service/src/main/java/com/jyd/microservice/currencyconversionservice/models/CurrencyConversion.java
+++ currency-conversion-service/src/main/java/com/jyd/microservice/currencyconversionservice/models/CurrencyConversion.java
@@ -1,4 +1,24 @@
 package com.jyd.microservice.currencyconversionservice.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class CurrencyConversion {
+
+    private Long id;
+    private String from;
+    private String to;
+    private BigDecimal quality;
+    private BigDecimal conversionMultiple;
+    private BigDecimal totalCalculatedAmount;
+    private String environment;
 }
